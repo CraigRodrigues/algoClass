eb52e43139bddfce1ee6ad97cd9932322a29ffb6
Craig Rodrigues <PagingCraig@gmail.com>
2017-01-31 17:59:52 -0500

    Implement Selection sort

--- sorting-algorithms/selection.js
+++ sorting-algorithms/selection.js
@@ -15,10 +15,43 @@ swap with element at beggining of unsorted subarray
 sorted portion has now grown:
 [ 1 2 3 4|5 7 9 ]
 
+[3, 1, 2, 4, 9, 5, 7]
+
 *** Exercises
 
 - Implement selection sort
 - Identify time complexity
+
+*/
+
+function selectionSort(arr) {
+
+// Find the smallest element in array
+// Swap it with the first element of our sub-array
+// Continue this until we have no elements left in our sub-array to look at
+
+  let pivot = 0;
+  while (pivot < arr.length) {
+    // find smallest value in unsorted part of array
+    let minIndex = pivot;
+    for (let i = pivot; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+    }
+    // once we have found our smallest value then swap taht value with the front of the unsorted list
+    let temp = arr[pivot];
+    arr[pivot] = arr[minIndex];
+    arr[minIndex] = temp;
+
+    // increment the pivot
+    pivot++
+  }
+
+  return arr;
+}
+
+/*
 
 Stable Variant
 - Implement as a stable sort - rather than swapping, the minimum value is inserted into the first position and all other items are shifted one to the right. How does this impact performance?
