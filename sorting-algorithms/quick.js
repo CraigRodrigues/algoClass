5fdce805b5d859c55a372c5f3218e8db91f5cbac
Craig Rodrigues <PagingCraig@gmail.com>
2017-02-05 18:28:51 -0800

    Implement swap and partition parts of Quick sort

--- sorting-algorithms/quick.js
+++ sorting-algorithms/quick.js
@@ -21,3 +21,43 @@ Variants:
 - Implement a multi-pivot quicksort (ex: partition into 3 subarrays using 2 pivots)
 
 */
+
+const quickSort = function (collection, left = 0, right = collection.length - 1) {
+
+};
+
+const swap = function (collection, firstIndex, secondIndex) {
+  let temp = collection[firstIndex];
+  collection[firstIndex] = collection[secondIndex];
+  collection[secondIndex] = temp;
+
+  return collection;
+};
+
+const partition = function (collection, left, right) {
+  // pivot is the middle of the left and right indexes
+  let pivot = collection[Math.floor((right + left) / 2)];
+  let i = left;
+  let j = right;
+
+  // Keep looping until our points meet
+  while (i <= j) {
+    // move left point until a value greater than pivot is found
+    while (collection[i] < pivot) {
+      i++;
+    }
+    // move right pointer until a value less than the pivot is found
+    while (collection[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(collection, i, j);
+      i++;
+      j--;
+    }
+  }
+  console.log(collection);
+  return i;
+};
+
+console.log(partition([4, 2, 6, 5, 3, 9], 0, 5));
